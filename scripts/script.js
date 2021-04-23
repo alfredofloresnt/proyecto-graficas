96c1f86af5d75564687a76e08c890f18c023a7d0
Andres Orozco <andresorocast.aoc@gmail.com>
2021-04-23 18:27:32 -0500

    cambio de modelo

--- scripts/script.js
+++ scripts/script.js
@@ -1,10 +1,14 @@
 import * as THREE from 'https://threejsfundamentals.org/threejs/resources/threejs/r125/build/three.module.js';
 import {OrbitControls} from 'https://threejsfundamentals.org/threejs/resources/threejs/r125/examples/jsm/controls/OrbitControls.js';
 import {GLTFLoader} from 'https://threejsfundamentals.org/threejs/resources/threejs/r125/examples/jsm/loaders/GLTFLoader.js';
+
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
 const loader = new GLTFLoader();
 const renderer = new THREE.WebGLRenderer();
+const controls = new OrbitControls( camera, renderer.domElement );
+
+
 camera.position.z = 5;
 renderer.setSize(window.innerWidth, window.innerHeight);
 renderer.shadowMap.enabled = true;
@@ -12,7 +16,7 @@ renderer.shadowMap.type = THREE.PCFSoftShadowMap;
 document.body.appendChild(renderer.domElement);
 
 // Crear luz //
-let light = new THREE.PointLight( 0xffffff, 1, 100 );
+let light = new THREE.PointLight( 0xffffff, 1, 1000 );
 light.position.set( 0, 12, 0 );
 light.castShadow = true;            // default false
 light.shadow.mapSize.width = 1024;  // default 512
@@ -21,23 +25,29 @@ light.shadow.camera.near = 2;       // default 0.5
 light.shadow.camera.far = 100;      // default 500
 scene.add( light );
 
+
 // Helper //
 const helper = new THREE.CameraHelper( light.shadow.camera );
 //scene.add( helper );
 
 
 
-/*
+
 let room;
-loader.load('../models/room.glb', function (gltf) {
+loader.load('../models/room2.glb', function (gltf) {
     room = gltf.scene;
-    room.castShadow = true; 
-    room.receiveShadow = true; 
     scene.add(room);
+    // model.traverse( function ( object ) {
+
+    //     if ( object.isMesh ) object.castShadow = true;
+
+    // } );
+    // room.castShadow = true; 
+    // room.receiveShadow = true; 
 }, undefined, function (error) {
     console.errr(error);
 });
-*/
+
 
 // Crear plano //
 var texture = new THREE.TextureLoader().load( 'img/floor.jpg' );
@@ -58,19 +68,25 @@ const cubeMaterial = new THREE.MeshBasicMaterial({ color: 0xffffff });
 const cube = new THREE.Mesh(cubeGeometry, cubeMaterial);
 cube.scale.set(0.5,0.5,0.5);
 cube.position.y = 1;
+cube.position.z = 0;
 cube.castShadow = true;
 cube.receiveShadow = true;
 scene.add(cube);
 
 
-camera.position.z = 4;
+camera.position.z = 20;
 camera.position.y = 1.10;
+camera.rotation.y = Math.PI/2;
+controls.update();
 
 const animate = function () {
     requestAnimationFrame(animate);
     cube.rotation.y += 0.1;
     cube.rotation.z += 0.1;
+    controls.update();
     renderer.render(scene, camera);
+
+
 };
 
 animate();
